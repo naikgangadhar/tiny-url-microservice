ac922de4fb11766410fde8eb159b88ed24b0a2b2
Ganga <naikgangadhar@github.com>
2021-09-24 12:18:15 +0530

    master | laptop change data

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const http = require("http");
 const codecClass = require('./codec');
+require('dotenv').config();
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(process.env.DATABASE , process.env.USERNAME , process.env.PASSWORD ,{
+    host : 'localhost',
+    dialect : 'mysql'
+});
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+  
 const PORT = 3000;
 const httpserver = async function(req , res){
     res.writeHead(200);
---
+++ migrations/20210922055432-create-url-mapping.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('UrlMappings', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      url: {
+        type: Sequelize.STRING(2083)
+      },
+      tiny_url: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('UrlMappings');
+  }
+};
